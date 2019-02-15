7efa30e950f5085d7c1b74e65ac002568ba80625
dbozhenko <dbozhenko@vidscale.com>
2019-02-15 17:32:57 +0200

    Create class to manage sessions

--- tests/test_add_contact.py
+++ tests/test_add_contact.py
@@ -13,7 +13,7 @@ def app(request):
 
 
 def test_add_contact(app):
-    app.login("admin", "secret")
+    app.session.login("admin", "secret")
     app.create_new_contact(Contact(firstname="Dara", middlename="Test", lastname="Bozhenko", username="dbozhenko",
                                         title="TestContact", company="Company", address="Address,105/10",
                                         homephone="+38048578993", mobilephone="+38902849903", workphone="+98248585985",
@@ -21,14 +21,14 @@ def test_add_contact(app):
                                         email3="db3@example.com", homepage="test.db.com", birthday="4",
                                         birthmonth="July", birthyear="1989", aday="26", amonth="November", ayear="2000",
                                         address2="Second Adress, 35/9", phone2="+924050485498", notes="Testing notest"))
-    app.logout()
+    app.session.logout()
 
 
 def test_add_empty_contact(app):
-    app.login("admin", "secret")
+    app.session.login("admin", "secret")
     app.create_new_contact(Contact(firstname="", middlename="", lastname="", username="", title="", company="",
                                             address="", homephone="", mobilephone="", workphone="", fax="", email="",
                                             email2="", email3="", homepage="", birthday="", birthmonth="-",
                                             birthyear="",
                                             aday="", amonth="-", ayear="", address2="", phone2="", notes=""))
-    app.logout()
+    app.session.logout()
--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -13,12 +13,12 @@ def app(request):
 
 
 def test_add_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="test", header="testgroup", footer="groupfooter"))
-    app.logout()
+    app.session.logout()
 
 
 def test_add__empty_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="", header="", footer=""))
-    app.logout()
+    app.session.logout()
