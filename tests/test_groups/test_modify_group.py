74d815585ae7c7e40769b62e1bb7898e7f569d6b
dbozhenko <dbozhenko@vidscale.com>
2019-02-25 21:33:44 +0200

    Added methods for filling group and contact forms

--- tests/test_groups/test_modify_group.py
+++ tests/test_groups/test_modify_group.py
@@ -1,7 +1,13 @@
 from model.group import Group
 
 
-def test_modify_group(app):
+def test_modify_group_name(app):
     app.session.login(username="admin", password="secret")
-    app.group.modify_first_group(Group(name="edited name", header="edited header", footer="edited footer"))
+    app.group.modify_first_group(Group(name="edited name"))
+    app.session.logout()
+
+
+def test_modify_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(header="edited header"))
     app.session.logout()
