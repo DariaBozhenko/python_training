d77a0fef01730e23e85da6df0aa4f28514390c60
dbozhenko <dbozhenko@vidscale.com>
2019-03-16 00:26:50 +0200

    added browser, baseurl and adminpass options for command line

--- conftest.py
+++ conftest.py
@@ -1,18 +1,22 @@
 import pytest
 from fixture.application import Application
 
+
 fixture = None
 
 
 @pytest.fixture
 def app(request):
     global fixture
+    browser = request.config.getoption("--browser")
+    base_url = request.config.getoption("--baseurl")
+    admin_pass = request.config.getoption("--adminpass")
     if fixture is None:
-        fixture = Application()
+        fixture = Application(browser=browser, base_url=base_url)
     else:
         if not fixture.is_valid():
-            fixture = Application()
-    fixture.session.ensure_login(username="admin", password="secret")
+            fixture = Application(browser=browser, base_url=base_url)
+    fixture.session.ensure_login(username="admin", password=admin_pass)
     return fixture
 
 
@@ -24,3 +28,9 @@ def stop(request):
 
     request.addfinalizer(fin)
     return fixture
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="firefox")
+    parser.addoption("--baseurl", action="store", default="http://localhost/addressbook/index.php")
+    parser.addoption("--adminpass", action="store")
